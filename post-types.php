47662a9b5ead748d9a6405d944d260a57c984aeb
lewisdonovan <lewis.donovan@sonymusic.com>
2017-06-21 14:56:03 +0100

    Create post-types.php

---
+++ post-types.php
@@ -0,0 +1,43 @@
+<?php
+
+//LOOP THROUGH POST TYPES AND ADD ACF FIELDS TO API
+add_action( 'init', 'sme_hook_all_post_types', 10 );
+function sme_hook_all_post_types(){
+
+  //Get all the post types
+  global $wp_post_types;
+  $post_types = array_keys( $wp_post_types );
+
+  //Loop through each one
+  foreach ($post_types as $post_type) {
+
+    //Add a filter for this post type
+    add_filter( 'rest_prepare_'.$post_type, function($data, $post, $request){
+
+      //Get the response data
+      $response_data = $data->get_data();
+
+      //Bail early if there's an error
+      if ( $request['context'] !== 'view' || is_wp_error( $data ) ) {
+          return $data;
+      }
+
+      //Get all fields
+      $fields = get_fields($post->ID);
+      //If we have fields...
+      if ($fields){
+        //Loop through them...
+        foreach ($fields as $field_name => $value){
+          //Set the meta
+          $response_data[$field_name] = $value;
+        }
+      }
+
+      //Commit the API result var to the API endpoint
+      $data->set_data( $response_data );
+      return $data;
+    }, 10, 3);
+  }
+}
+
+?>
