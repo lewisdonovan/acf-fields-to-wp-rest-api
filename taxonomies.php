dc038d55bcedcfb1449204341c287ce283b7593c
lewisdonovan <lewis.donovan@sonymusic.com>
2017-06-21 14:56:45 +0100

    Create taxonomies.php

---
+++ taxonomies.php
@@ -0,0 +1,41 @@
+<?php
+
+//LOOP THROUGH TAXONOMIES AND ADD ACF FIELDS TO API
+add_action( 'init', 'sme_hook_all_terms', 10 );
+function sme_hook_all_terms(){
+  $taxonomies = array_keys( get_taxonomies() );
+
+  foreach ($taxonomies as $taxonomy) {
+
+    add_filter( 'rest_prepare_'.$taxonomy, function($data, $term, $request){
+
+      //Get the response data
+      $response_data = $data->get_data();
+
+      //Bail early if there's an error
+      if ( $request['context'] !== 'view' || is_wp_error( $data ) ) {
+          return $data;
+      }
+
+      //Get all fields
+      $fields = get_fields('term_'.$term->term_id);
+      //var_dump($fields);
+      //If we have fields...
+      if ($fields){
+        //Loop through them...
+        foreach ($fields as $field_name => $value){
+          //Set the meta
+          $response_data[$field_name] = $value;
+        }
+      }
+
+      //Commit the API result var to the API endpoint
+      $data->set_data( $response_data );
+      return $data;
+
+    }, 10, 3);
+
+  }
+}
+
+?>
