6de7466b326a94c820971668a7eefef28ba168fb
lewisdonovan <lewis.donovan@sonymusic.com>
2017-06-21 14:55:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: ACF Fields to WP REST API
+Plugin URI: https://github.com/lewisdonovan/acf-fields-to-wp-rest-api
+Description: Programmatically add data from Advanced Custom Fields to the Wordpress REST API
+Author: Lewis Donovan
+Version: 1.0
+Author URI: http://lewisdonovan.co.uk
+*/
+
+include 'post-types.php';
+include 'taxonomies.php';
+
+?>
